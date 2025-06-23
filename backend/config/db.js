43313a41416de5e748fa26229284dfb0529b163c
ovez khan <khanovez257@gmail.com>
2025-06-24 03:02:55 +0530

    Fix mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then((data) => {
       console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
